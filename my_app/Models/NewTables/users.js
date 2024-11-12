89c29aa811063754cfeb30c41f3859ba3abefc6a
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-12 09:47:58 +0500

    Adding env File

--- my_app/Models/NewTables/users.js
+++ my_app/Models/NewTables/users.js
@@ -7,5 +7,27 @@ users.init(
         userId : {
             primaryKey : true ,
             type : DataTypes.STRING()
+        } ,
+        username : {
+            unique : true ,
+            type   : DataTypes.STRING(),
+            allowNull : false
+        } ,
+        name :{
+            type : DataTypes.STRING(),
+            allowNull : false
+        } ,
+        password : {
+            type : DataTypes.STRING() ,
+            allowNull : false
         }
-    },{})
+    },
+    {
+        name : "my_Table" ,
+        timestamps : true ,
+        paranoid :true ,
+    
+    }
+)
+
+module.exports = users ;
